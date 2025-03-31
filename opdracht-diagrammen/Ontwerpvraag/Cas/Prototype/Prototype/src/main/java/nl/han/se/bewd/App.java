92ae35d39b57a17c4ef71e19841ee5deef83c2b2
Cas Meulenbelt <c.meulenbelt@student.han.nl>
2025-03-31 22:12:20 +0200

    Updated: Prototype

--- opdracht-diagrammen/Ontwerpvraag/Cas/Prototype/Prototype/src/main/java/nl/han/se/bewd/App.java
+++ opdracht-diagrammen/Ontwerpvraag/Cas/Prototype/Prototype/src/main/java/nl/han/se/bewd/App.java
@@ -2,15 +2,8 @@ package nl.han.se.bewd;
 
 public class App {
     public static void main(String[] args) {
-        AuthServiceAdapter authAdapter = new AuthServiceAdapter();
-        TransportServiceAdapter transportAdapter = new TransportServiceAdapter();
-
-        DatabaseService databaseService = new DatabaseService();
-
-        Backend backend = new Backend(authAdapter, databaseService);
-
-        authAdapter.callService("Auth123", "user@example.com");
-
-
+        System.out.println("=== Using AuthServiceAdapter ===");
+        AuthService authService = new AuthService(new AuthServiceAdapter());
+        authService.authenticateUser("user@example.com");
     }
 }
