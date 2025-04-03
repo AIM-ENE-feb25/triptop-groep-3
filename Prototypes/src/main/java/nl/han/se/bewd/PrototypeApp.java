172b7e0a21a757c5da314893d392cc9d83a2dd3e
Cas Meulenbelt <c.meulenbelt@student.han.nl>
2025-04-03 11:56:45 +0200

    Deleted: Overbodige witregels/commentaar

--- Prototypes/src/main/java/nl/han/se/bewd/PrototypeApp.java
+++ Prototypes/src/main/java/nl/han/se/bewd/PrototypeApp.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrototypeApp {
     public static void main(String[] args) {
         SpringApplication.run(PrototypeApp.class, args);
-//        iExternalServiceAdapter authAdapter = new AuthServiceAdapter();
-//        iExternalServiceAdapter transportAdapter = new TransportServiceAdapter();
-//
-//        AuthService authService = new AuthService(authAdapter, new AuthRepository());
-//        TransportService transportService = new TransportService(transportAdapter);
+
     }
 }
